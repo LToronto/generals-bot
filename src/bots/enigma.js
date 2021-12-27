4d77a16358ae65f5c1109f09c70150341906f8f8
Lyle Toronto <Lyle.Toronto@FamilySearch.org>
2021-12-27 16:03:32 -0700

    refactor enigma

--- src/bots/enigma.js
+++ src/bots/enigma.js
@@ -1,10 +1,10 @@
 /**
  * This bot randomly moves its largest army to a random foggy location
- * 
+ *
  */
 const OPENING_FIRST_MOVE_THRESHOLD = 24;  //24 game ticks = 12 armies
 
-const TERRAIN_ATTACKABLE = -1; // empty or city or enemy
+const TERRAIN_EMPTY = -1; // empty or city or enemy
 // const TERRAIN_MTN = -2;
 const TERRAIN_FOG = -3;  //empty or swamp or occupied
 // const TERRAIN_FOG_MTN = -4; //city or mnt
@@ -14,11 +14,12 @@ let ai = {
   game: undefined, // TODO: If we make the data arrays into sets, we don't have to worry about pushing repeat state, like known city locations
   intel: {
     map: [],
+    locations: [],
     attackQueue: [],
     opponents: [],
     myScore: {total: 0, tiles: 0, lostArmies: false, lostTerritory: false},
     myTopArmies: [], // The map locations we own and have a minimum number of armies available.
-    attackableTerritories: [], // The map locations we can see that are free to conquer.
+    emptyTerritories: [], // The map locations we can see that are free to conquer.
     foggedTerritories: [],
     visibleOpponentTerritories: [],
     unexploredTerritories: [], // The set of remaining board indices we have not yet explored while searching for generals.
@@ -54,88 +55,87 @@ let ai = {
    */
   determineMoves: function () {
 
-    if (this.game.turn < OPENING_FIRST_MOVE_THRESHOLD) {
-      // Build armies at first
-      return;
-    }
+    // if (this.game.turn <= OPENING_FIRST_MOVE_THRESHOLD) {
+    //   // Build armies at first
+    //   return;
+    // }
 
     if (this.game.intel.attackQueue.length > 0) {
-      let nextAttacker = this.game.intel.attackQueue.attackerIndex
-      if (this.game.armies[nextAttacker] < 2) {
+      let nextAttacker = this.makeLocationObject(this.game.intel.attackQueue[0].attackerIndex)
+      if (nextAttacker.armies < 2 || !nextAttacker.isMine) {
         this.game.intel.attackQueue = []
       }
     }
 
-    // if (this.game.intel.attackQueue.length < 1) {
-    //   queueEasyWins(this.game.intel.myTopArmies)
-    // }
+    if (this.game.intel.attackQueue.length < 1) {
+      this.queueEasyWins(this.game.intel.myTopArmies)
+    }
 
     // Don't queue up new stuff if there is stuff to do.
     if (this.game.intel.attackQueue.length < 1) {
       // move largest army to random empty Territory
       const random = Math.floor(Math.random() * this.game.intel.foggedTerritories.length);
-      let nextMoveIndexes = this.findPath(this.game.intel.myArmies[0].locationIdx, this.game.intel.foggedTerritories[random])
-      for (let i = nextMoveIndexes.length - 1; i > 0; i--) {
+      let nextMoveLocations = this.findPath(this.game.intel.myArmies[0], this.game.intel.foggedTerritories[random])
+      for (let i = nextMoveLocations.length - 1; i > 0; i--) {
         this.game.intel.attackQueue.push({
-          mode: "Random", attackerIndex: nextMoveIndexes[i], targetIndex: nextMoveIndexes[i - 1], sendHalf: false
+          mode: "Random", attackerIndex: nextMoveLocations[i].idx, targetIndex: nextMoveLocations[i - 1].idx, sendHalf: false
         });
       }
     }
-
   },
 
-  // queueEasyWins(availableArmies) {
-  //   for(let i = 0; i<availableArmies.length; i++) {
-  //     let neighbors = this.findNeighbors(availableArmies[i])
-  //     for(let n = 0; n<neighbors.length; n++) {
-  //       if(neighbors[n])
-  //     }
-  //   }
-  //
-  //
-  //   this.game.intel.attackQueue.push({
-  //     mode: "Creep", attackerIndex: nextMoveIndexes[i], targetIndex: nextMoveIndexes[i - 1], sendHalf: false
-  //   });
-  // }
+  queueEasyWins(availableArmies) {
+    for(let i = 0; i<availableArmies.length; i++) {
+      let neighbors = this.findNeighbors(availableArmies[i])
+      for(let n = 0; n<neighbors.length; n++) {
+        if(neighbors[n].attackable && availableArmies[i].armies > neighbors[n].armies+1) {
+          this.game.intel.attackQueue.push({mode: "EasyWin", attackerIndex: availableArmies[i].idx, targetIndex: neighbors[n].idx, sendHalf: false})
+          return //Only queue one for each attacker
+        }
+      }
+    }
+  },
 
-
-  findNeighbors(locationIdx) {
-    const row = Math.floor(locationIdx / this.game.mapWidth);
-    const col = locationIdx % this.game.mapWidth;
+  findNeighbors(location) {
+    const row = Math.floor(location.idx / this.game.mapWidth);
+    const col = location.idx % this.game.mapWidth;
     let neighbors = []
     if (this.intel.map[row - 1] && this.intel.map[row - 1][col]) {
-      neighbors.push((row - 1) * this.game.mapWidth + col)
+      neighbors.push(this.makeLocationObject((row - 1) * this.game.mapWidth + col))
     }
     if (this.intel.map[row + 1] && this.intel.map[row + 1][col]) {
-      neighbors.push((row + 1) * this.game.mapWidth + col)
+      neighbors.push(this.makeLocationObject((row + 1) * this.game.mapWidth + col))
     }
     if (this.intel.map[row] && this.intel.map[row][col - 1]) {
-      neighbors.push(row * this.game.mapWidth + (col - 1))
+      neighbors.push(this.makeLocationObject(row * this.game.mapWidth + (col - 1)))
     }
     if (this.intel.map[row] && this.intel.map[row][col + 1]) {
-      neighbors.push(row * this.game.mapWidth + (col + 1))
+      neighbors.push(this.makeLocationObject(row * this.game.mapWidth + (col + 1)))
     }
     return neighbors
   },
 
-  findPath: function (locationIdx, targetLocation) {
-    let distanceMap = this.createDistanceMap(locationIdx)
+  findPath: function (location, targetLocation) {
+    let distanceMap = this.createDistanceMap(location)
     let pathIndexes = this.findShortestPath(distanceMap, targetLocation)
-    console.log("New Path:" + pathIndexes)
+    console.log("New Path:" + JSON.stringify(pathIndexes))
     return pathIndexes;
   },
 
-  findShortestPath: function (distanceMap, path) {
-    if (!Array.isArray(path)) {
-      path = [path]
+  findShortestPath: function (distanceMap, targetLocationOrPath) {
+    let path = []
+    if (Array.isArray(targetLocationOrPath)) {
+      path = targetLocationOrPath
+    } else {
+      path.push(targetLocationOrPath)
     }
     let lastInPath = path[path.length - 1]
     // Map Path Distance
-    let possiblePath = this.findNeighbors(lastInPath)
+    let neighborLocation = this.findNeighbors(lastInPath)
     let chosenPath = lastInPath;
-    for (let i = 0; i < possiblePath.length; i++) {
-      if (distanceMap[possiblePath[i]] < distanceMap[chosenPath]) {
-        chosenPath = possiblePath[i]
+    for (let i = 0; i < neighborLocation.length; i++) {
+      if (distanceMap[neighborLocation[i].idx] < distanceMap[chosenPath.idx]) {
+        chosenPath = neighborLocation[i]
       }
     }
     if (chosenPath !== lastInPath) {
@@ -146,23 +146,23 @@ let ai = {
     return path
   },
 
-  createDistanceMap: function (locationIdx) {
+  createDistanceMap: function (location) {
     let distanceMap = []
-    let queue = [locationIdx]
-    distanceMap[locationIdx] = 0
+    let queue = [location]
+    distanceMap[location.idx] = 0
 
     while (queue.length > 0) {
       let currentLocation = queue.shift();
-      let currentDistance = distanceMap[currentLocation]
+      let currentDistance = distanceMap[currentLocation.idx]
       if (currentDistance !== "M") {
         let neighbors = this.findNeighbors(currentLocation)
         for (let i = 0; i < neighbors.length; i++) {
-          if (typeof distanceMap[neighbors[i]] === 'undefined') {
+          if (typeof distanceMap[neighbors[i].idx] === 'undefined') {
             queue.push(neighbors[i])
-            if ((this.game.terrain[neighbors[i]] === TERRAIN_FOG || this.game.terrain[neighbors[i]] >= TERRAIN_ATTACKABLE)) {
-              distanceMap[neighbors[i]] = currentDistance + 1;
+            if (neighbors[i].terrain === TERRAIN_FOG || neighbors[i].terrain >= TERRAIN_EMPTY) {
+              distanceMap[neighbors[i].idx] = currentDistance + 1;
             } else {
-              distanceMap[neighbors[i]] = "M"
+              distanceMap[neighbors[i].idx] = "M"
             }
           }
         }
@@ -191,10 +191,11 @@ let ai = {
       this.game.intel.unexploredTerritories = new Set([...Array(this.game.mapSize).keys()]);
     }
 
-    this.game.intel.attackableTerritories = [];
+    this.game.intel.emptyTerritories = [];
     this.game.intel.foggedTerritories = [];
     this.game.intel.visibleOpponentTerritories = [];
     this.game.intel.myArmies = [];
+    this.game.intel.myTopArmies = [];
     this.game.intel.totalAvailableArmyPower = 0;
 
     // Loop through map array once, and sort all data appropriately.
@@ -204,33 +205,42 @@ let ai = {
     //const index = row * this.game.mapWidth + col
     for (let row = 0; row < Math.floor(this.game.terrain.length / this.game.mapWidth); row++) {
       this.intel.map[row] = []
-      for (let column = 0; column < (this.game.terrain.length - 1) % this.game.mapWidth; column++) {
+      for (let column = 0; column <= (this.game.terrain.length - 1) % this.game.mapWidth; column++) {
         let idx = row * this.game.mapWidth + column
-        this.intel.map[row][column] = {terrain: this.game.terrain[idx], armies: this.game.armies[idx]}
+        this.intel.map[row][column] = this.makeLocationObject(idx)
       }
     }
     for (let idx = 0; idx < this.game.terrain.length; idx++) {
-      if (this.game.terrain[idx] === TERRAIN_ATTACKABLE) {
-        this.game.intel.attackableTerritories.push(idx);
-      } else  if (this.game.terrain[idx] === TERRAIN_FOG) {
-        this.game.intel.foggedTerritories.push(idx);
-      } else if (this.game.terrain[idx] === this.game.playerIndex) {
+      this.game.intel.locations[idx] = this.makeLocationObject(idx)
+      if (this.game.terrain[idx] === this.game.playerIndex) {
         this.game.intel.unexploredTerritories.delete(idx);
         if (this.game.armies[idx] > 1) {
-          this.game.intel.myArmies.push({locationIdx: idx, locationPower: this.game.armies[idx]});
           this.game.intel.totalAvailableArmyPower += this.game.armies[idx] - 1;
         }
-        if (this.game.armies[idx] >= USEFUL_ARMY_THRESHOLD) {
-          this.game.intel.myTopArmies.push({locationIdx: idx, locationPower: this.game.armies[idx]});
-        }
-      } else if (this.game.terrain[idx] > TERRAIN_ATTACKABLE && this.game.terrain[idx] !== this.game.playerIndex) {
-        this.game.intel.visibleOpponentTerritories.push(idx);
       }
     }
 
+    this.game.intel.emptyTerritories = this.game.intel.locations.filter((location) => location.terrain === TERRAIN_EMPTY);
+    this.game.intel.foggedTerritories = this.game.intel.locations.filter((location) => location.terrain === TERRAIN_FOG);
+    this.game.intel.myArmies = this.game.intel.locations.filter((location) => location.isMine);
+    this.game.intel.myTopArmies = this.game.intel.locations.filter((location) => location.isMine && location.armies >= USEFUL_ARMY_THRESHOLD);
+    this.game.intel.visibleOpponentTerritories = this.game.intel.locations.filter((location) => location.terrain > TERRAIN_EMPTY && location.terrain !== this.game.playerIndex);
+
     // sort() so that our largest army will be at the front of the array.
-    this.game.intel.myArmies.sort((a, b) => b.locationPower - a.locationPower);
+    this.game.intel.myArmies.sort((a, b) => b.armies - a.armies);
   },
+
+  makeLocationObject(locationIdx){
+    let terrain = this.game.terrain[locationIdx]
+    return {
+      idx: locationIdx,
+      armies: this.game.armies[locationIdx],
+      terrain: terrain,
+      isMine: terrain === this.game.playerIndex,
+      attackable: terrain === TERRAIN_EMPTY || (terrain > TERRAIN_EMPTY && terrain !== this.game.playerIndex)
+    }
+  }
+
 }
 
 export default ai;
